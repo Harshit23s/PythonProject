7ee388a8c54aec6a8cb2fc6121a002980a82c19a
Harshit Sharma <nonisharma2222@gmail.com>
2025-08-27 19:05:18 +0530

    push

---
+++ assignment1task1.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+add = a + b
+print("Addition :- " , add)
+sub = a-b
+print("Substraction :- ",sub)
+mul = a * b
+print("Multiplication :-",mul)
+div = a / b
+print("Division :-",div)
+
