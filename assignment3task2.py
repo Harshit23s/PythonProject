91ebc6e465bbc0833b0efe80a75322965f82afb9
Harshit Sharma <nonisharma2222@gmail.com>
2025-09-08 18:17:26 +0530

    Push

---
+++ assignment3task2.py
@@ -0,0 +1,14 @@
+import math
+
+
+num = float(input("Enter a number: "))
+
+
+square_root = math.sqrt(num)
+log_value = math.log(num)
+sine_value = math.sin(num)
+
+# Show results
+print("Square root of", num, "is:", square_root)
+print("Natural logarithm of", num, "is:", log_value)
+print("Sine of", num, "is:", sine_value)
